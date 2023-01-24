2600a8d020b3230c3afbea64ffbd83807a06070c
Aabesov <aabesov2005@mail.ru>
2023-01-24 19:32:17 +0600

    feat: created authentication

---
+++ authentication/send_mail.py
@@ -0,0 +1,21 @@
+from django.core.mail import send_mail
+from django.template.loader import render_to_string
+from django.utils.html import strip_tags
+from django.conf import settings
+
+
+def send_message_to_email(email, activation_code):
+    context = {
+        "email": email,
+        "domain": " http://127.0.0.1:8000",
+        "activation_code": activation_code
+    }
+    message = render_to_string("email.html", context)
+    finally_message = strip_tags(message)
+    send_mail(
+        'Активация аккаунта',
+        finally_message,
+        settings.EMAIL_HOST_USER,
+        [email, ],
+        html_message=message,
+    )
---
+++ tabaco_shop/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls.static import static
+from django.contrib import admin
+from django.urls import path, include
+
+from authentication.models import User
+from tabaco_shop import settings
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/v1/accounts/', include("authentication.urls")),
+]
